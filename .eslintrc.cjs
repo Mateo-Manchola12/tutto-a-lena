00fafcb4379f00912b4abdae8dc826b3eefd1438
Teo <mmanchola12temporal@gmail.com>
2026-01-16 12:56:12 +0100

    chore(eslint): configure flat ESLint for Astro + TypeScript with Astro parser and globals

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/**
+ * ESLint config for Astro + TypeScript
+ */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['**/*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      extends: ['plugin:astro/recommended'],
+      rules: {
+        // Buenas prácticas Astro
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-set-html-directive': 'warn',
+      },
+    },
+    {
+      files: ['**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      },
+    },
+  ],
+}
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import astro from 'eslint-plugin-astro'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+
+export default [
+  {
+    ...js.configs.recommended,
+    languageOptions: { globals: globals.browser },
+  },
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    files: ['**/*.cjs', '**/*.mjs', 'astro.config.mjs', 'eslint.config.js'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+      'no-case-declarations': 'off',
+      'astro/no-conflict-set-directives': 'error',
+      'astro/no-set-html-directive': 'warn',
+    },
+  },
+]
