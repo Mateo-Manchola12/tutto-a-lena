7455560fed54ca7a9b878f20dd82a03ea8bb4529
Teo <mmanchola12temporal@gmail.com>
2026-04-08 18:31:53 +0200

    fix: correct business info retrieval by adjusting data access and ensuring proper type conversion

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,9 +14,9 @@ const db = getFirestore(app);
 export async function getBusinessInfo() {
     const currentRef = doc(db, "builds", "current");
     const currentSnap = await getDoc(currentRef);
-    const currentBuild = currentSnap.data()?.build;
+    const currentBuild = currentSnap.data();
 
-    const ref = doc(db, "business_info", currentBuild.lastContentId);
+    const ref = doc(db, "business_info", currentBuild?.lastContentId.toString());
     const snap = await getDoc(ref);
 
     return snap.data() as BusinessInfo;
