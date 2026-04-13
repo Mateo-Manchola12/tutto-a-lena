25ae3f4ed136430c792974d9854f3796a4df1223
Teo <mmanchola12temporal@gmail.com>
2026-04-13 20:48:54 +0200

    feat(config): add WP_DOMAIN env var and export WP_API_BASE constant

--- src/constants/static_paths.ts
+++ src/constants/static_paths.ts
@@ -2,9 +2,10 @@ import { loadEnv } from 'vite'
 
 type StaticPath = string
 
-const { PUBLIC_SITE_URL, PUBLIC_INDEXABLE } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), '')
+const { PUBLIC_SITE_URL, PUBLIC_INDEXABLE, WP_DOMAIN } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), '')
 
 export const DOMAIN_NAME: StaticPath = PUBLIC_SITE_URL
 export const INDEXABLE: boolean = PUBLIC_INDEXABLE === 'true'
+export const WP_API_BASE: StaticPath = WP_DOMAIN ?? ''
 
 export const CTA_TARGET: StaticPath = ''
